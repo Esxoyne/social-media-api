82ec5a1c38be88ccf6c11013f7304b7704ce4c2e
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-10-12 19:46:54 +0300

    Create Profile model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
+import os
+import uuid
+
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 from django.utils.translation import gettext as _
+from django_countries.fields import CountryField
 
 from .managers import UserManager
 
@@ -15,3 +21,43 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.email
+
+
+def generate_file_name(info, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(info)}-{uuid.uuid4()}{extension}"
+
+    return filename
+
+
+def profile_picture_file_path(instance, filename):
+    filename = generate_file_name(instance.user.username, filename)
+
+    return os.path.join("uploads/profile_pictures/", filename)
+
+
+class Profile(models.Model):
+    class GenderChoices(models.TextChoices):
+        FEMALE = "female", "Female"
+        MALE = "male", "Male"
+        OTHER = "other", "Other"
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    picture = models.ImageField(
+        upload_to=profile_picture_file_path,
+        null=True,
+        blank=True,
+    )
+    bio = models.CharField(max_length=160, blank=True)
+    gender = models.CharField(
+        max_length=32,
+        choices=GenderChoices.choices,
+        blank=True,
+    )
+    country = CountryField(blank=True)
+
+    def __str__(self) -> str:
+        return str(self.user)
