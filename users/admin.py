cf7934c9ca4c19859a7ce332e228ea1ce8abb020
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-10-12 03:03:59 +0300

    Register User model in the admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "last_login",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+
+admin.site.unregister(Group)
