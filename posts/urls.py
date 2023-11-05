e1239c3ca6d9cd3fae3bfc0c7d5818d6ae9ee06d
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-11-05 21:18:34 +0200

    Create posts URLconf

---
+++ posts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from posts.views import PostViewSet
+
+
+router = routers.DefaultRouter()
+router.register("", PostViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "posts"
