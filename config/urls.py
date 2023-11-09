e9f7c26fa06f54c8df9c5f9002ef310775556d82
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-11-09 16:07:52 +0200

    Add URLs for schema

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,27 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/posts/", include("posts.urls", namespace="posts")),
     path("api/users/", include("users.urls", namespace="users")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
