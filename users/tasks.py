670078628e0927446e7cbdfa17c6e5075edbaeeb
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-11-19 20:21:20 +0200

    Create Celery task for flushing expired tokens

---
+++ users/tasks.py
@@ -0,0 +1,9 @@
+from celery import shared_task
+
+from django.core.management import call_command
+
+
+@shared_task
+def flush_expired_tokens() -> None:
+    call_command("flushexpiredtokens")
+    print("Expired tokens flushed.")
